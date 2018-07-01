6c93c80ed8af7bff9e2e42cae2689e1946fdb655
Jihaad Pienaar <jihaad_pienaar@hotmail.com>
2018-07-01 12:43:00 +0200

    S3L56-Added Levelswithces

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 	[SerializeField] float rcsThrust = 100f;
 	[SerializeField] float mainThrust = 100f;
 
+	enum State{ Alive, Dying, Transcending};
+	State state = State.Alive;
 
 	Rigidbody rigidBody;
 	AudioSource audioSource;
@@ -17,19 +18,37 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Thrust();
-		Rotate();
+		if (state == State.Alive) {
+			Thrust();
+			Rotate();
+		}
 	}
 
 	void OnCollisionEnter(Collision collision) {
+		if (state != State.Alive) return;
+		
 		switch(collision.gameObject.tag) {
 			case "Friendly":
-				print("Safu");
+				// Do nothing
+				break;
+			case "Finish":
+				state = State.Transcending;
+				Invoke("LoadNextLevel", 1f);
 				break;
 			default:
-				print("Dead");
+				state = State.Dying;
+				Invoke("LoadFirstLevel", 1f);
 				break;
 		}
+	}
+
+	private void LoadNextLevel() {
+		// TODO: Allow for more scenes
+		SceneManager.LoadScene(1);
+	}
+
+	private void LoadFirstLevel() {
+		SceneManager.LoadScene(0);
 	}
 
 	private void Thrust() {
