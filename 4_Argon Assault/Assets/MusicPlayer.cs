7409e5dc40ee5b9df4408adfeb30704e086a6c92
Jihaad Pienaar <jihaad_pienaar@hotmail.com>
2018-07-02 18:32:37 +0200

    Added splash screen and Music player

---
+++ 4_Argon Assault/Assets/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	void Awake () {
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadNextScene", 2f);
+	}
+	
+	private void LoadNextScene() {
+		SceneManager.LoadScene(1);
+	}
+}
